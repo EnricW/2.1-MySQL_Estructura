d1f9bc439911bc8b7226c688553c87e1ae01e9c3
EnricW <reiben815@gmail.com>
2024-12-09 11:56:46 +0100

    Add files via upload

---
+++ spotify.sql
@@ -0,0 +1,419 @@
+CREATE DATABASE  IF NOT EXISTS `spotify` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `spotify`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: spotify
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `album`
+--
+
+DROP TABLE IF EXISTS `album`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `album` (
+  `album_id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `release_year` int NOT NULL,
+  `cover_image` varchar(255) DEFAULT NULL,
+  `artist_id` int NOT NULL,
+  PRIMARY KEY (`album_id`),
+  KEY `artist_id` (`artist_id`),
+  CONSTRAINT `album_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `album`
+--
+
+LOCK TABLES `album` WRITE;
+/*!40000 ALTER TABLE `album` DISABLE KEYS */;
+INSERT INTO `album` VALUES (1,'25',2015,'25.jpg',1),(2,'Divide',2017,'divide.jpg',2),(3,'1989',2014,'1989.jpg',3);
+/*!40000 ALTER TABLE `album` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `album_favorite`
+--
+
+DROP TABLE IF EXISTS `album_favorite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `album_favorite` (
+  `favorite_album_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `album_id` int NOT NULL,
+  `marked_date` date NOT NULL,
+  PRIMARY KEY (`favorite_album_id`),
+  KEY `user_id` (`user_id`),
+  KEY `album_id` (`album_id`),
+  CONSTRAINT `album_favorite_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `album_favorite_ibfk_2` FOREIGN KEY (`album_id`) REFERENCES `album` (`album_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `album_favorite`
+--
+
+LOCK TABLES `album_favorite` WRITE;
+/*!40000 ALTER TABLE `album_favorite` DISABLE KEYS */;
+INSERT INTO `album_favorite` VALUES (1,1,1,'2024-12-03'),(2,2,2,'2024-11-22'),(3,3,3,'2024-11-05');
+/*!40000 ALTER TABLE `album_favorite` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `artist`
+--
+
+DROP TABLE IF EXISTS `artist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `artist` (
+  `artist_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`artist_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `artist`
+--
+
+LOCK TABLES `artist` WRITE;
+/*!40000 ALTER TABLE `artist` DISABLE KEYS */;
+INSERT INTO `artist` VALUES (1,'Adele','adele.jpg'),(2,'Ed Sheeran','ed_sheeran.jpg'),(3,'Taylor Swift','taylor_swift.jpg');
+/*!40000 ALTER TABLE `artist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payment` (
+  `payment_id` int NOT NULL AUTO_INCREMENT,
+  `subscription_id` int NOT NULL,
+  `payment_method_id` int NOT NULL,
+  `order_number` varchar(255) NOT NULL,
+  `payment_date` date NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`payment_id`),
+  UNIQUE KEY `order_number` (`order_number`),
+  KEY `subscription_id` (`subscription_id`),
+  KEY `payment_method_id` (`payment_method_id`),
+  CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`subscription_id`) REFERENCES `subscription` (`subscription_id`),
+  CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`payment_method_id`) REFERENCES `payment_method` (`payment_method_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment`
+--
+
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+INSERT INTO `payment` VALUES (1,1,1,'ORD123','2024-01-01',9.99),(2,2,2,'ORD124','2024-06-01',19.99),(3,3,3,'ORD125','2024-03-01',14.99);
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment_method`
+--
+
+DROP TABLE IF EXISTS `payment_method`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payment_method` (
+  `payment_method_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `method_type` enum('credit_card','paypal') NOT NULL,
+  `card_number` varchar(255) DEFAULT NULL,
+  `expiry_month` int DEFAULT NULL,
+  `expiry_year` int DEFAULT NULL,
+  `security_code` varchar(255) DEFAULT NULL,
+  `paypal_username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`payment_method_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `payment_method_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment_method`
+--
+
+LOCK TABLES `payment_method` WRITE;
+/*!40000 ALTER TABLE `payment_method` DISABLE KEYS */;
+INSERT INTO `payment_method` VALUES (1,1,'credit_card','4111111111111111',12,2025,'123',NULL),(2,2,'paypal',NULL,NULL,NULL,NULL,'bob_paypal'),(3,3,'credit_card','5555555555554444',6,2026,'456',NULL);
+/*!40000 ALTER TABLE `payment_method` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `playlist`
+--
+
+DROP TABLE IF EXISTS `playlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `playlist` (
+  `playlist_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `song_count` int DEFAULT '0',
+  `creation_date` date NOT NULL,
+  `is_deleted` tinyint(1) DEFAULT '0',
+  `deletion_date` date DEFAULT NULL,
+  PRIMARY KEY (`playlist_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `playlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `playlist`
+--
+
+LOCK TABLES `playlist` WRITE;
+/*!40000 ALTER TABLE `playlist` DISABLE KEYS */;
+INSERT INTO `playlist` VALUES (1,1,'My Favorites',3,'2024-12-01',0,NULL),(2,2,'Chill Vibes',5,'2024-11-15',0,NULL),(3,3,'Workout Mix',8,'2024-10-30',1,NULL);
+/*!40000 ALTER TABLE `playlist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `playlist_song`
+--
+
+DROP TABLE IF EXISTS `playlist_song`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `playlist_song` (
+  `playlist_song_id` int NOT NULL AUTO_INCREMENT,
+  `playlist_id` int NOT NULL,
+  `song_id` int NOT NULL,
+  `added_by_user_id` int NOT NULL,
+  `added_date` date NOT NULL,
+  PRIMARY KEY (`playlist_song_id`),
+  KEY `playlist_id` (`playlist_id`),
+  KEY `song_id` (`song_id`),
+  KEY `playlist_song_ibfk_3` (`added_by_user_id`),
+  CONSTRAINT `playlist_song_ibfk_1` FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`playlist_id`),
+  CONSTRAINT `playlist_song_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `song` (`song_id`),
+  CONSTRAINT `playlist_song_ibfk_3` FOREIGN KEY (`added_by_user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `playlist_song`
+--
+
+LOCK TABLES `playlist_song` WRITE;
+/*!40000 ALTER TABLE `playlist_song` DISABLE KEYS */;
+INSERT INTO `playlist_song` VALUES (1,1,1,1,'2024-12-01'),(2,2,2,2,'2024-11-16'),(3,3,3,3,'2024-11-01');
+/*!40000 ALTER TABLE `playlist_song` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `related_artist`
+--
+
+DROP TABLE IF EXISTS `related_artist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `related_artist` (
+  `artist_id` int NOT NULL,
+  `related_artist_id` int NOT NULL,
+  PRIMARY KEY (`artist_id`,`related_artist_id`),
+  KEY `related_artist_id` (`related_artist_id`),
+  CONSTRAINT `related_artist_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`),
+  CONSTRAINT `related_artist_ibfk_2` FOREIGN KEY (`related_artist_id`) REFERENCES `artist` (`artist_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `related_artist`
+--
+
+LOCK TABLES `related_artist` WRITE;
+/*!40000 ALTER TABLE `related_artist` DISABLE KEYS */;
+INSERT INTO `related_artist` VALUES (3,1),(1,2),(2,3);
+/*!40000 ALTER TABLE `related_artist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `song`
+--
+
+DROP TABLE IF EXISTS `song`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `song` (
+  `song_id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `duration` int NOT NULL,
+  `play_count` int DEFAULT '0',
+  `album_id` int NOT NULL,
+  PRIMARY KEY (`song_id`),
+  KEY `song_ibfk_1` (`album_id`),
+  CONSTRAINT `song_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `album` (`album_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `song`
+--
+
+LOCK TABLES `song` WRITE;
+/*!40000 ALTER TABLE `song` DISABLE KEYS */;
+INSERT INTO `song` VALUES (1,'Hello',295,1000000,1),(2,'Shape of You',233,2000000,2),(3,'Blank Space',231,1500000,3);
+/*!40000 ALTER TABLE `song` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `song_favorite`
+--
+
+DROP TABLE IF EXISTS `song_favorite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `song_favorite` (
+  `favorite_song_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `song_id` int NOT NULL,
+  `marked_date` date NOT NULL,
+  PRIMARY KEY (`favorite_song_id`),
+  KEY `user_id` (`user_id`),
+  KEY `song_id` (`song_id`),
+  CONSTRAINT `song_favorite_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `song_favorite_ibfk_2` FOREIGN KEY (`song_id`) REFERENCES `song` (`song_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `song_favorite`
+--
+
+LOCK TABLES `song_favorite` WRITE;
+/*!40000 ALTER TABLE `song_favorite` DISABLE KEYS */;
+INSERT INTO `song_favorite` VALUES (1,1,1,'2024-12-02'),(2,2,2,'2024-11-20'),(3,3,3,'2024-11-01');
+/*!40000 ALTER TABLE `song_favorite` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subscription`
+--
+
+DROP TABLE IF EXISTS `subscription`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subscription` (
+  `subscription_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `start_date` date NOT NULL,
+  `renewal_date` date NOT NULL,
+  `payment_method_id` int NOT NULL,
+  PRIMARY KEY (`subscription_id`),
+  KEY `user_id` (`user_id`),
+  KEY `subscription_ibfk_2` (`payment_method_id`),
+  CONSTRAINT `subscription_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `subscription_ibfk_2` FOREIGN KEY (`payment_method_id`) REFERENCES `payment_method` (`payment_method_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subscription`
+--
+
+LOCK TABLES `subscription` WRITE;
+/*!40000 ALTER TABLE `subscription` DISABLE KEYS */;
+INSERT INTO `subscription` VALUES (1,1,'2024-01-01','2025-01-01',1),(2,2,'2024-06-01','2025-06-01',2),(3,3,'2024-03-01','2025-03-01',3);
+/*!40000 ALTER TABLE `subscription` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `birth_date` date NOT NULL,
+  `gender` enum('male','female') NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `zip_code` varchar(20) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'alice@example.com','hashed_password_1','alice','1990-05-14','female','USA','10001'),(2,'bob@example.com','hashed_password_2','bob','1988-11-22','male','UK','SW1A 1AA'),(3,'charlie@example.com','hashed_password_3','charlie','1995-07-30','male','Canada','M5H 2N2');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_artist`
+--
+
+DROP TABLE IF EXISTS `user_artist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_artist` (
+  `user_artist_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `artist_id` int NOT NULL,
+  PRIMARY KEY (`user_artist_id`),
+  KEY `user_id` (`user_id`),
+  KEY `artist_id` (`artist_id`),
+  CONSTRAINT `user_artist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `user_artist_ibfk_2` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_artist`
+--
+
+LOCK TABLES `user_artist` WRITE;
+/*!40000 ALTER TABLE `user_artist` DISABLE KEYS */;
+INSERT INTO `user_artist` VALUES (1,1,1),(2,2,2),(3,3,3);
+/*!40000 ALTER TABLE `user_artist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-09 11:54:00
